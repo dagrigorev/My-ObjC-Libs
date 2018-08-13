0347fa43e50622dfc9486f3b196a7220a1724bbf
Dmitry Grigorev <dagrigorev@dmitrys-macbook-pro.local>
2018-08-13 18:33:57 +0300

    Initial Commit

---
+++ GraphLib/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  GraphLib
+//
+//  Created by Dmitry Grigorev on 13.08.18.
+//  Copyright © 2018 Dmitry Grigorev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
